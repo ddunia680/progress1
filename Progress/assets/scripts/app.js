eb346d7129bf462ffa0892c56e9061d3d51ffaa5
Dunia Dunia <ddunia680@gmail.com>
2022-01-07 20:15:22 +0300

    OOP approach

--- Progress/assets/scripts/app.js
+++ Progress/assets/scripts/app.js
@@ -1,219 +1,216 @@
-const accessmodal = document.getElementById('add-modal');
-const newProcurationButton = document.querySelector('header button');
-const backdrop = document.getElementById('backdrop');
-const vaccineType = document.querySelector('select');
-const vaccineDetails = document.querySelectorAll('input');
-const cancelButtonOnModal = accessmodal.querySelector('.btn--passive');
-const addButtonOnModal = cancelButtonOnModal.nextElementSibling;
-const confirmCancelModal = document.getElementById('delete-modal');
-const textEntrySection = document.getElementById('entry-text');
-const listOfVaccines = document.getElementById('movie-list');
-const addVaccine = document.getElementById('button-addVaccine');
-const astraBar = document.getElementById('astra');
-const pfizerBar = document.getElementById('pfiz');
-const johnsonBar = document.getElementById('john');
-const sectionUI = document.getElementById('quantity-levels');
-const injectionValue = 2;
+class Procurations {
+    accessmodal = document.getElementById('add-modal');
+    newProcurationButton = document.querySelector('header button');
+    backdrop = document.getElementById('backdrop');
+    cancelButtonOnModal = document.getElementById('cancelBtn');
 
 
+    toggleBackgroundOpacity(){
+        this.backdrop.classList.toggle('visible');
+    }
 
-
-let startingBarLevels = 0;
-let currentAstraBarLevel = startingBarLevels;
-let currentPfizerBarLevel = startingBarLevels;
-let currentJohnsonBarLevel = startingBarLevels;
-let marginStartingValue = -80;
-
-
-adjustBars = (minLife) => {
-    astraBar.min = minLife;
-    astraBar.value = minLife;
-    astraBar.max = 100;
-
-    pfizerBar.min = minLife;
-    pfizerBar.value = minLife;
-
-    johnsonBar.min = minLife;
-    johnsonBar.value = minLife;
-  };
-
-
-adjustBars(startingBarLevels);
-
-
-const vaccine = [];
-
-
-const toggleBackgroundOpacity = () => {
-    backdrop.classList.toggle('visible');
-}
-
-const makeModalAppear = () => {
-    accessmodal.classList.add('visible');
-    toggleBackgroundOpacity();
-}
-
-const clearInputsInModal = () => {
-    vaccineType.value = 'select';
-    for(el of vaccineDetails) {
+    clearInputsInModal() {
+        const proc = new NewProcuration();
+        proc.vaccineType.value = 'select';
+    for(const el of proc.vaccineDetails) {
         el.value = '';
     }
-}
+    }
 
-const closeModal = () => {
-    clearInputsInModal();
-    toggleBackgroundOpacity();
-    accessmodal.classList.remove('visible');
-
-}
-
-const closeDelectionModal = () => {
-    toggleBackgroundOpacity();
-    confirmCancelModal.classList.remove('visible');
-};
-
-const launchDeletionModal = (vacId) => {
-
-    toggleBackgroundOpacity();
-    confirmCancelModal.classList.add('visible');
-
-    let confirmDeletionBtn = confirmCancelModal.querySelector('.btn--danger');
-    const cancelOperationBtn = confirmCancelModal.querySelector('.btn--passive');
-
-    cancelOperationBtn.removeEventListener('click', closeDelectionModal);
-    confirmDeletionBtn.replaceWith(confirmDeletionBtn.cloneNode(true));
-
-    confirmDeletionBtn = confirmCancelModal.querySelector('.btn--danger');
-
-    cancelOperationBtn.addEventListener('click', closeDelectionModal);
-    confirmDeletionBtn.addEventListener('click', deleteVaccination.bind(null, vacId));
+    makeModalAppear() {
+        this.accessmodal.classList.add('visible');
+        this.toggleBackgroundOpacity();
+    }
     
+    closeModal() {
+        this.clearInputsInModal();
+        this.toggleBackgroundOpacity();
+        this.accessmodal.classList.remove('visible');
+    }
 
+    callEvents() {
+        this.newProcurationButton.addEventListener('click',this.makeModalAppear.bind(this));
+        this.backdrop.addEventListener('click', this.closeModal.bind(this));
+        this.cancelButtonOnModal.addEventListener('click', this.closeModal.bind(this));
+    }
 }
 
-const updateUinterface = () => {
-    if(vaccine.length === 0) {
-        textEntrySection.style.display = 'block';
-    }else{
-        textEntrySection.style.display = 'none';
-    }
-};
+class NewProcuration {
+    newVacData = document.createElement('li');
+    vaccineType = document.querySelector('select');
+    vaccineDetails = document.querySelectorAll('input');
+    listOfVaccines = document.getElementById('movie-list');
+    addButtonOnModal = document.getElementById('button-addVaccine');
+    vaccine = [];
 
-const deleteVaccination = (vacId) => {
-    let vaccineIndex = 0;
-    for(vac of vaccine) {
-        if(vac.id === vacId){
-            break;
+    VaccinationData(id, type, coreDetail, age) {
+        this.newVacData.className = 'movie-element';
+        this.newVacData.innerHTML = `
+            <div class = "movie-element__image">
+               <h1>${type}</h1>
+            </div>
+            <div class = "movie-element-info">
+               <h2>${coreDetail}</2>
+               <p>${age} years old</p>
+            </div>
+            `;
+
+        this.listOfVaccines.append(this.newVacData);            
+    }
+
+    AddVacData() {
+        this.vaccineType.value;
+        const coreDetail = this.vaccineDetails[0].value;
+        const age = this.vaccineDetails[1].value;
+
+        const vacData = {
+            id: Math.random().toString(),
+            type: this.vaccineType.value,
+            coreDetail: coreDetail,
+            age: age,
+        };
+        this.VaccinationData(vacData.id, vacData.type, vacData.coreDetail, vacData.age);
+        this.vaccine.push(vacData);
+        console.log(this.vaccine);
+        const prodAv = new ProductsAvailability
+        prodAv.injection(this.vaccineType.value);
+        const iu = new IUManager();
+        iu.dealWithMargin();
+        const procu = new Procurations();
+        procu.closeModal();
+        procu.clearInputsInModal();
+        iu.updateUinterface(this.vaccine);
+        prodAv.checkForAvailability();
+    }
+
+
+    callEvents() {
+        this.addButtonOnModal.removeEventListener('click', this.AddVacData.bind(this));
+        this.addButtonOnModal.replaceWith(this.addButtonOnModal.cloneNode(true));
+
+        let addButtonOnModal = document.getElementById('button-addVaccine');
+        addButtonOnModal.addEventListener('click', this.AddVacData.bind(this));
+    }
+    
+}
+
+class IUManager {
+    
+    dealWithMargin() {
+        const sectionUI = document.getElementById('quantity-levels');
+        let marginStartingValue = -80;
+        marginStartingValue = marginStartingValue -25;
+        let toBeAppended = marginStartingValue.toString() + 'px';
+        sectionUI.style.marginTop = toBeAppended;
+    }
+
+    updateUinterface(vaccine) {
+        this.vaccine = vaccine;
+        const textEntrySection = document.getElementById('entry-text');
+        if(vaccine.length === 0) {
+            textEntrySection.style.display = 'block';
+        }else{
+            textEntrySection.style.display = 'none';
         }
-        vaccineIndex++;
-    }
-    vaccine.splice(vaccineIndex, 1);
-    listOfVaccines.children[vaccineIndex].remove();
-    closeModal();
-    closeDelectionModal();
-    toggleBackgroundOpacity();
-    updateUinterface();
-};
-
-const addVaccinationData = () => {
-    vaccineType.value;
-    const coreDetail = vaccineDetails[0].value;
-    const age = vaccineDetails[1].value;
-
-    const vacData = {
-        id: Math.random().toString(),
-        type: vaccineType.value,
-        coreDetail: coreDetail,
-        age: age,
-    };
-    vaccine.push(vacData);
-    console.log(vaccine);
-    injection(vaccineType.value);
-    dealWithMargin();
-    closeModal();
-    clearInputsInModal();
-    VaccinationData(vacData.id, vacData.type, vacData.coreDetail, vacData.age);
-    updateUinterface();
-    checkForAvailability();
-
-}
-
-const VaccinationData = (id, type, coreDetail, age) => {
-    const newVacData = document.createElement('li');
-    newVacData.className = 'movie-element';
-    newVacData.innerHTML = `
-    <div class = "movie-element__image">
-       <h1>${type}</h1>
-    </div>
-    <div class = "movie-element-info">
-       <h2>${coreDetail}</2>
-       <p>${age} years old</p>
-    </div>
-    `;
-    newVacData.addEventListener('click', launchDeletionModal.bind(null, id));
-    listOfVaccines.append(newVacData);
-};
-
-const dealWithMargin = () => {
-
-    marginStartingValue = marginStartingValue -25;
-    let toBeAppended = marginStartingValue.toString() + 'px';
-    sectionUI.style.marginTop = toBeAppended;
-
-}
-
-const AstraZenecaInjection = (consumedQuantity) => {
-    const quantity = consumedQuantity;
-    astraBar.value = +astraBar.value + quantity;
-    return quantity;
-}
-
-const pfizerInjection = (consumedQuantity) => {
-    const quantity = consumedQuantity;
-    pfizerBar.value = +pfizerBar.value + quantity;
-    return quantity;
-}
-
-const johnsonInjection = (consumedQuantity) => {
-    const quantity = consumedQuantity;
-    johnsonBar.value = +johnsonBar.value + quantity;
-    return quantity;
-}
-
-
-const injection = (injectionType) => {
-    if(injectionType === 'AstraZeneca') {
-        AstraZenecaInjection(injectionValue);
-        // sectionUI.style.marginTop = ((marginStartingValue + 30) + 'px').toString();
-    } else if(injectionType === 'Pfizer'){
-        pfizerInjection(injectionValue);
-    }else if(injectionType === 'Johnson&Johnson'){
-        johnsonInjection(injectionValue);
     }
 }
 
-const checkForAvailability = () => {
-if(astraBar.value === 100){
-    alert('The AstraZeneca is now out of stock');
-    const selectUI = document.getElementById('title');
-    selectUI.childNodes[3].style.display = 'none';
+class ProductsAvailability {
+    astraBar = document.getElementById('astra');
+    pfizerBar = document.getElementById('pfiz');
+    johnsonBar = document.getElementById('john');
+    injectionValue = 30;
+
+    startingBarLevels = 0;
+    currentAstraBarLevel = this.startingBarLevels;
+    currentPfizerBarLevel = this.startingBarLevels;
+    currentJohnsonBarLevel = this.startingBarLevels;
+    marginStartingValue = -80;
+    
+    constructor() {}
+
+    adjustBars(minLife) {
+        this.astraBar.min = minLife;
+        this.astraBar.value = minLife;
+        this.astraBar.max = 100;
+        
+        this.pfizerBar.min = minLife;
+        this.pfizerBar.value = minLife;
+        
+        this.johnsonBar.min = minLife;
+        this.johnsonBar.value = minLife;
+    }
+
+    // setBars(){
+    //     this.adjustBars(this.startingBarLevels);
+    // }
+
+
+    AstraZenecaInjection(consumedQuantity) {
+            const quantity = consumedQuantity;
+            this.astraBar.value = +this.astraBar.value + quantity;
+            return quantity;
+        }
+        
+    pfizerInjection(consumedQuantity) {
+            const quantity = consumedQuantity;
+            this.pfizerBar.value = +this.pfizerBar.value + quantity;
+            return quantity;
+        }
+        
+    johnsonInjection(consumedQuantity) {
+            const quantity = consumedQuantity;
+            this.johnsonBar.value = +this.johnsonBar.value + quantity;
+            return quantity;
+        }
+        
+        
+    injection(injectionType) {
+            if(injectionType === 'AstraZeneca') {
+                this.AstraZenecaInjection(this.injectionValue);
+                // sectionUI.style.marginTop = ((marginStartingValue + 30) + 'px').toString();
+            } else if(injectionType === 'Pfizer'){
+                this.pfizerInjection(this.injectionValue);
+            }else if(injectionType === 'Johnson&Johnson'){
+                this.johnsonInjection(this.injectionValue);
+            }
+        }
+        
+    checkForAvailability() {
+        if(this.astraBar.value === 100){
+            alert('The AstraZeneca is now out of stock');
+            const selectUI = document.getElementById('title');
+            selectUI.childNodes[3].style.display = 'none';
+        }
+        
+        if(this.pfizerBar.value === 100){
+            alert('The Pfizer is now out of stock');
+            const selectUI = document.getElementById('title');
+            selectUI.childNodes[5].style.display = 'none';
+        }
+        
+        if(this.johnsonBar.value === 100){
+            alert('The Johnson&Johnson is now out of stock');
+            const selectUI = document.getElementById('title');
+            selectUI.childNodes[7].style.display = 'none';
+        }
+    }
+    
 }
 
-if(pfizerBar.value === 100){
-    alert('The Pfizer is now out of stock');
-    const selectUI = document.getElementById('title');
-    selectUI.childNodes[5].style.display = 'none';
+
+class App {
+    static init() {
+        const proc = new Procurations();
+        proc.callEvents();
+        const newProc = new NewProcuration();  
+        newProc.callEvents();
+        new ProductsAvailability();     
+    }
 }
 
-if(johnsonBar.value === 100){
-    alert('The Johnson&Johnson is now out of stock');
-    const selectUI = document.getElementById('title');
-    selectUI.childNodes[7].style.display = 'none';
-}
-};
 
-newProcurationButton.addEventListener('click', makeModalAppear);
-cancelButtonOnModal.addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
-addVaccine.addEventListener('click', addVaccinationData);
+App.init();
+
+
 
